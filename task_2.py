22a44ef25653ba86ace366e447867fdb6a1611da
Olena Bielova <belova.elena11@gmail.com>
2025-03-14 09:11:32 +0200

    task 2

--- task_2.py
+++ task_2.py
@@ -3,7 +3,9 @@ import random
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
         return []
-    print(sorted(random.sample(range(min, max + 1), quantity))) 
-    return sorted(random.sample(range(min, max + 1), quantity))
+    
+    numbers = sorted(random.sample(range(min, max + 1), quantity))
+    
+    return numbers
 
 print(get_numbers_ticket(1, 49, 6)) 
